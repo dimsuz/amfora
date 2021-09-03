af52d99e720ad0cb9e6cd941547ac814b492fe13
Dmitry Suzdalev <mail@dimsuz.ru>
2021-09-04 01:27:27 +0200

    Send Login using apollo

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,16 @@ import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.5.21"
     id("org.jetbrains.compose") version "1.0.0-alpha3"
-    id("com.apollographql.apollo3") version "3.0.0-alpha03"
+    id("com.apollographql.apollo") version "2.5.9"
 }
 
 group = "ru.dimsuz"
@@ -26,8 +32,8 @@ apollo {
 dependencies {
     implementation(compose.desktop.currentOs)
 
-    implementation("com.apollographql.apollo:apollo-runtime:3.0.0-alpha03")
-    implementation("com.apollographql.apollo:apollo-coroutines-support:3.0.0-alpha03")
+    implementation("com.apollographql.apollo:apollo-runtime:2.5.9")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.9")
 }
 
 tasks.withType<KotlinCompile>() {
--- src/main/kotlin/ru/dimsuz/amfora/screen/LoginScreen.kt
+++ src/main/kotlin/ru/dimsuz/amfora/screen/LoginScreen.kt
@@ -1,6 +1,5 @@
 package ru.dimsuz.amfora.screen
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,21 +10,31 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.toInput
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.dimsuz.amfora.LoginMutation
 import ru.dimsuz.amfora.Screen
 
 class LoginScreen : Screen {
-
   @Composable
   override fun Content() {
     var state by remember { mutableStateOf(State()) }
+    val scope = rememberCoroutineScope()
 
     Column(
       modifier = Modifier.fillMaxSize(),
@@ -44,7 +53,7 @@ class LoginScreen : Screen {
         onValueChange = { state = state.copy(password = it) }
       )
       Spacer(modifier = Modifier.height(16.dp))
-      Button(onClick = {}) {
+      Button(onClick = { scope.login(state.login.text, state.password.text) }) {
         Text(text = "Login")
       }
     }
@@ -55,4 +64,23 @@ class LoginScreen : Screen {
     val login: TextFieldValue = TextFieldValue(),
     val password: TextFieldValue = TextFieldValue(),
   )
+
+  val apolloClient = ApolloClient.builder()
+    .serverUrl("https://stoplight.io/graphql")
+    .build()
+
+  private fun CoroutineScope.login(email: String, password: String) {
+    launch(Dispatchers.IO) {
+      try {
+        val result = apolloClient.mutate(LoginMutation(
+          email.toInput(),
+          password.toInput(),
+          workspaceIntegrationId = 167699
+        )).await()
+        println("response: ${result.data}")
+      } catch (e: ApolloException) {
+        e.printStackTrace()
+      }
+    }
+  }
 }
